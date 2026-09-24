098f362af3b0212bfeaac33bf118d9b657324e18
3wasfnjd <44668762+3wasfnjd@users.noreply.github.com>
2026-09-25 00:31:04 +0300

    Update dynamic title to موتري

--- sources/Game/Title.js
+++ sources/Game/Title.js
@@ -85,7 +85,7 @@ export class Title
             }
 
             
-            document.title = 'موتري 2' + title.join('')
+            document.title = 'موتري' + title.join('')
         }
     }
 }
